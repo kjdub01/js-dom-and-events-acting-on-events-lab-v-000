48f665cbf5a347a3b2561a1e160dc8789defee21
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-23 20:51:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ function addNewElementAsLi() {
 
 function addNewLiOnClick() {
   let submit = document.querySelector('input[type = "submit"]')
-  submit.addEventListener('click', function(e))
+  submit.addEventListener('click', function(e){
+    
+  });
 }
 
 function clearEmployeeListOnLinkClick() {
