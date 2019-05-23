7b3a1d567773bcc4b4510c3ece831db02e37c237
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-23 20:50:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function addNewElementAsLi() {
 }
 
 function addNewLiOnClick() {
-  let submit = document.querySelector('input[type = submit]')
+  let submit = document.querySelector('input[type = "submit"]')
+  
 }
 
 function clearEmployeeListOnLinkClick() {
