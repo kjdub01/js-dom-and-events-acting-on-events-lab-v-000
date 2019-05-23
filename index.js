7a5ffd028f975f304a0f07c149ecb40596a9a6e7
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-23 20:52:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function addNewElementAsLi() {
 function addNewLiOnClick() {
   let submit = document.querySelector('input[type = "submit"]')
   submit.addEventListener('click', function(e){
+    addNewElementAsLi();
     
   });
 }
